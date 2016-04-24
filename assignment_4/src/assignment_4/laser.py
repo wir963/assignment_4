c845b13ac974d2c7b1d3a577a7383815f4591fb1
Welles Robinson <wir963@gmail.com>
2016-04-24 13:58:08 -0400

    more debugging

--- assignment_4/src/assignment_4/laser.py
+++ assignment_4/src/assignment_4/laser.py
@@ -96,7 +96,9 @@ def get_last_point(x0, y0, angle, the_map):
   # else: # angle < 2*math.pi
   #   # max_x or min_y
   max_x = the_map.info.width
+  rospy.loginfo("width of the map is %i" %(max_x))
   max_y = the_map.info.height
+  rospy.loginfo("height of the map is %i" %(max_y))
   min_x = 0
   min_y = 0
   angle = update_angle(angle, 0)
