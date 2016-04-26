bbbe71e495b3af56dcb13ed5cbe439b77069861f
Welles Robinson <wir963@gmail.com>
2016-04-26 10:10:23 -0400

    use .25 and .1 for variance

--- assignment_4/src/assignment_4/particle.py
+++ assignment_4/src/assignment_4/particle.py
@@ -87,7 +87,7 @@ def resample(particles_weighted, n_particles, the_map):
       current_particle += 1
       (score, particle) = particles_normalized[current_particle]
       cumulative_score += score
-    particle = new_particle(particle, .5, .5, the_map)
+    particle = new_particle(particle, .25, .1, the_map)
     particles.append(particle)
     current_score += gap
   assert len(particles) == n_particles
